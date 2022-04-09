404b47bdc86f20400966caaca2889b1d4cfc3d04
BessanaKatherine <besanakatherine@gmail.com>
2022-04-09 16:30:58 +0800

    My Hobbies added

--- git-python-Besana.py
+++ git-python-Besana.py
@@ -4,4 +4,8 @@ print{"May 14,2001"}
 print{"MSC"}
 print{"Bachelor of Science Information Technology"}
 print{"2-C "}
-
+print{"Cooking"}
+print{"Singing"}
+print{"Dancing"}
+print{"Designing"}
+print{"Playing Badminton"}
